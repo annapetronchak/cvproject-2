8f403a840bd07bd8b6786c5b98d1f71d8983e6e0
Anna Petronchak <anna.petronchak@gmail.com>
2016-12-23 23:26:59 +0000

    Initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#content").fullpage({
+    css3: true,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Skills', 'Projects', 'References', 'Contacts'],
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    responsiveWidth: 660,
+    responsiveHeight: 400
+  });
+});
